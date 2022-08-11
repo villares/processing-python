5409ad41d5631836989a17edfa24e36efc20f104
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-08-10 22:14:29 -0300

    KinecticType

--- other-examples/3D/Typography/KineticType/KineticType.py
+++ other-examples/3D/Typography/KineticType/KineticType.py
@@ -2,13 +2,11 @@
  Kinetic Type
  by Zach Lieberman.
  Adapted to Python by Jonathan Feinberg
- Using push() and pop() to define the curves of the lines of type.
+ Using push() and pop() to define the curves of the line_s of type.
 """
-from java.lang import System
-System.setProperty("jogl.disable.openglcore", "false")
 
 words = [
-    "sometimes it's like", "the lines of text", "are so happy",
+    "sometimes it's like", "the line_s of text", "are so happy",
     "that they want to dance", "or leave the page or jump",
     "can you blame them?", "living on the page like that",
     "waiting to be read..."
@@ -28,20 +26,20 @@ def draw():
     translate(-200, -50, -450)
     rotate_y(0.3)
 
-    # Now animate every line object & draw it...
+    # Now animate every line_ object & draw it...
     for i in range(len(words)):
         f1 = sin((i + 1.0) * (millis() / 10000.0) * TWO_PI)
         f2 = sin((8.0 - i) * (millis() / 10000.0) * TWO_PI)
-        line = words[i]
+        line_ = words[i]
         push_matrix()
         translate(0.0, i*75, 0.0)
-        for j in range(len(line)):
+        for j in range(len(line_)):
             if j != 0:
-                translate(text_width(line[j - 1]) * 75, 0.0, 0.0)
+                translate(text_width(line_[j - 1]) * 75, 0.0, 0.0)
             rotate_y(f1 * 0.005 * f2)
             push_matrix()
             scale(75.0)
-            text(line[j], 0.0, 0.0)
+            text(line_[j], 0.0, 0.0)
             pop_matrix()
         pop_matrix()
     pop_matrix()
