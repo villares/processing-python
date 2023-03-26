ecb530bed5c736b83872d82df15f1056720f7d58
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-03-25 23:22:47 -0300

    swing fixes

--- swing_examples/field_input/field_input.py
+++ swing_examples/field_input/field_input.py
@@ -8,7 +8,9 @@ def setup():
     else:
         print(answer)  # Canceled dialog will print None
 
-
 def input(question='', suggestion=''):
     from javax.swing import JOptionPane
-    return JOptionPane.showInputDialog(None, question, suggestion)
+    result = JOptionPane.showInputDialog(None, question, suggestion,
+                                       # JOptionPane.PLAIN_MESSAGE, # no icon
+                                       )
+    return str(result) if result is not None else None
--- swing_examples/yes_no_pane/yes_no_pane.py
+++ swing_examples/yes_no_pane/yes_no_pane.py
@@ -1,6 +1,5 @@
 from javax.swing import JOptionPane
 
-
 def setup():
     result = yes_no_pane("oi?", "tudo bem?")
     if result == JOptionPane.YES_OPTION:
@@ -12,37 +11,37 @@ def setup():
 
 
 def yes_no_pane(title, message):
+    """
+    messageType - Defines the style of the message.
+        # 2:OK/Cancel 0:Yes/No  1:Y/N/Cancel -1:OK
+        The Look and Feel manager may lay out the dialog differently
+        depending on this value, and will often provide a default icon.
+        The possible values are:
+            JOptionPane.ERROR_MESSAGE
+            JOptionPane.INFORMATION_MESSAGE
+            JOptionPane.WARNING_MESSAGE
+            JOptionPane.QUESTION_MESSAGE # question mark (default)
+            JOptionPane.PLAIN_MESSAGE  # No icon
+    optionType - Defines the set of option buttons that appear at the bottom of the dialog box:
+            JOptionPane.DEFAULT_OPTION (OK/0 only, but you can still close the window... -1)
+            JOptionPane.YES_NO_OPTION
+            JOptionPane.YES_NO_CANCEL_OPTION
+            JOptionPane.OK_CANCEL_OPTION
+    Returned results:
+            0 JOptionPane.YES_OPTION
+            1 JOptionPane.NO_OPTION
+            2 JOptionPane.CANCEL_OPTION
+            0 JOptionPane.OK_OPTION
+           -1 JOptionPane.CLOSED_OPTION
+    """
     from javax.swing import JOptionPane
     return JOptionPane.showConfirmDialog(None,
                                          message,
                                          title,
                                          JOptionPane.YES_NO_OPTION,
-                                         # JOptionPane.QUESTION_MESSAGE, #
-                                         # default
+                                         # JOptionPane.QUESTION_MESSAGE,
                                          JOptionPane.PLAIN_MESSAGE  # No icon
                                          )
 
 
-"""
-messageType - Defines the style of the message.
-    # 2:OK/Cancel 0:Yes/No  1:Y/N/Cancel -1:OK
-    The Look and Feel manager may lay out the dialog differently
-    depending on this value, and will often provide a default icon.
-    The possible values are:
-        JOptionPane.ERROR_MESSAGE
-        JOptionPane.INFORMATION_MESSAGE
-        JOptionPane.WARNING_MESSAGE
-        JOptionPane.QUESTION_MESSAGE # question mark (se não puser nada vai este)
-        JOptionPane.PLAIN_MESSAGE  # No icon
-        optionType - Defines the set of option buttons that appear at the bottom of the dialog box:
-        JOptionPane.DEFAULT_OPTION
-        JOptionPane.YES_NO_OPTION
-        JOptionPane.YES_NO_CANCEL_OPTION
-        JOptionPane.OK_CANCEL_OPTION
-    Results:
-        JOptionPane.YES_OPTION
-        JOptionPane.NO_OPTION
-        JOptionPane.CANCEL_OPTION
-        JOptionPane.OK_OPTION
-        JOptionPane.CLOSED_OPTION
-"""
+
